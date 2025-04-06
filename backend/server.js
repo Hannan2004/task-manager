9916d6c0cceec8c70aa71333eea0f1cfbd931921
Hannan2004 <2022.abdul.chougle@ves.ac.in>
2025-04-06 23:19:19 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -12,5 +12,5 @@ app.use(bodyParser.json());
 
 app.use('/api/tasks', require('./routes/taskRoutes'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
